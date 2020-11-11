9b278571aeb349995d0c5fc7146597ba3bd1c658
Kabirdas Henry <kbrdsk@gmail.com>
2020-11-11 11:30:08 -0800

    proper destructuring

--- src/templates/logic.js
+++ src/templates/logic.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-export default function Logic({description, work}) {
+export default function Logic({ data: { description, work } }) {
 	return (
 		<div>
 			<p className="puzzle-description">{description}</p>
-			<textarea
-				className="work"
-				disabled
-			>{work}</textarea>
+			<textarea className="work" disabled>
+				{work}
+			</textarea>
 		</div>
 	);
 }
