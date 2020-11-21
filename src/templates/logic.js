5213eda8fc4685063a6227837d3b91adb6d3bda2
Kabirdas Henry <kbrdsk@gmail.com>
2020-11-20 18:01:40 -0800

    fixed work display when switching between logic puzzles

--- src/templates/logic.js
+++ src/templates/logic.js
@@ -3,10 +3,8 @@ import React from "react";
 export default function Logic({ data: { description, work } }) {
 	return (
 		<div className="logic">
-			<p className="puzzle-description">{description}</p>
-			<textarea className="work" disabled>
-				{work}
-			</textarea>
+			<p className="description">{description}</p>
+			<p className="work">{work}</p>
 		</div>
 	);
 }
