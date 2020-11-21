a979f7feeb31900c777c725dfaddf56f4c09a9e2
Kabirdas Henry <kbrdsk@gmail.com>
2020-11-20 16:51:07 -0800

    added student history

--- src/puzzle-display.js
+++ src/puzzle-display.js
@@ -17,6 +17,7 @@ const fetchDefaults = {
 export default function PuzzleDisplay(props) {
 	const [puzzleData, setPuzzleData] = useState(new Map());
 	const [fetchInfo, setFetchInfo] = useState(new Map());
+	const [studentHistory, setStudentHistory] = useState(new Map());
 	const activeStudents = useMemo(
 		() => props.students.filter((student) => student.active),
 		[props.students]
@@ -54,9 +55,53 @@ export default function PuzzleDisplay(props) {
 		[fetchInfo]
 	);
 
+	const updateHistory = useCallback(
+		async (student) => {
+			const { first, last } = student;
+			const url =
+				process.env.REACT_APP_API_URL +
+				`/students/${first}_${last}/activepuzzle`;
+			const response = await fetch(url, {
+				headers: { authorization: process.env.REACT_APP_INSTRUCTOR_PW },
+			});
+			const {
+				puzzleName,
+				puzzleId,
+				...puzzleData
+			} = await response.json();
+			if (!puzzleName) return;
+			setStudentHistory((oldHistory) => {
+				const studentHistory = oldHistory.get(student) || {};
+				const puzzleHistory = studentHistory[puzzleName] || {};
+				const instanceHistory = puzzleHistory[puzzleId] || [];
+				const isNewData =
+					!instanceHistory[0] ||
+					JSON.stringify({ puzzleId, ...puzzleData }) !==
+						JSON.stringify(instanceHistory[0].data);
+				if (isNewData) {
+					puzzleHistory[puzzleId] = [
+						{
+							data: { puzzleId, ...puzzleData },
+							timestamp: Date.now(),
+						},
+						...instanceHistory,
+					];
+					studentHistory[puzzleName] = puzzleHistory;
+					const newHistory = new Map(oldHistory);
+					newHistory.set(student, studentHistory);
+					return newHistory;
+				} else {
+					return oldHistory;
+				}
+			});
+		},
+		[setStudentHistory]
+	);
+
 	const refresh = useCallback(async () => {
 		const dataUpdates = await Promise.all(
 			activeStudents.map(async (student) => {
+				updateHistory(student);
 				const data = await fetchStudentPuzzleData(student);
 				return [student, data];
 			})
@@ -67,7 +112,7 @@ export default function PuzzleDisplay(props) {
 			return newPuzzleData;
 		});
 		console.log("refreshed");
-	}, [activeStudents, setPuzzleData, fetchStudentPuzzleData]);
+	}, [activeStudents, setPuzzleData, fetchStudentPuzzleData, updateHistory]);
 
 	//refresh puzzle data
 	useEffect(() => {
@@ -91,6 +136,48 @@ export default function PuzzleDisplay(props) {
 		const { puzzleName, ...data } = puzzleData.get(student) || {};
 		const render = puzzleComponents[puzzleName];
 		return render ? render(data) : null;
+	};
+
+	const renderHistory = (student) => {
+		const history = studentHistory.get(student);
+		if (!history) return null;
+		const { first, last } = student;
+		const sortedHistory = Object.keys(history)
+			.flatMap((key) =>
+				Object.keys(history[key]).map((instanceKey) => {
+					const {
+						data: { puzzleId },
+						timestamp,
+					} = history[key][instanceKey][0];
+					return { puzzleName: key, puzzleId, timestamp };
+				})
+			)
+			.sort(({ timestamp: a }, { timestamp: b }) =>
+				a > b ? -1 : 1
+			);
+		return (
+			<select
+				name="history-select"
+				className="history-select"
+				id={`history-${first}-${last}`}
+				onChange={(e) => {
+					const { puzzleName, puzzleId } = JSON.parse(e.target.value);
+					setStudentFetchInfo(student, "puzzleName", puzzleName);
+					setStudentFetchInfo(student, "puzzleId", puzzleId);
+				}}
+			>
+				{sortedHistory.map(({ puzzleName, puzzleId, timestamp }) => {
+					const time = new Date(timestamp).toLocaleTimeString();
+					return (
+						<option
+							value={JSON.stringify({ puzzleName, puzzleId })}
+						>
+							{puzzleId} {time}
+						</option>
+					);
+				})}
+			</select>
+		);
 	};
 
 	const renderPuzzleSelect = (student) => {
@@ -127,6 +214,7 @@ export default function PuzzleDisplay(props) {
 						<option value={id}>{id}</option>
 					))}
 				</select>
+				{renderHistory(student)}
 			</div>
 		);
 	};
@@ -138,22 +226,25 @@ export default function PuzzleDisplay(props) {
 		return (
 			<li key={`${first}_${last}`}>
 				<div className="student-header">
-					<span className="student-name">
+					<div className="student-name">
 						{capitalize(first)} {capitalize(last)}
-					</span>
-					<input
-						type="checkbox"
-						name="active"
-						id={`${first}_${last}-active`}
-						checked={activepuzzle ? true : false}
-						onChange={() =>
-							setStudentFetchInfo(
-								student,
-								"activepuzzle",
-								!activepuzzle
-							)
-						}
-					/>
+					</div>
+					<div>
+						Show Active Puzzle:
+						<input
+							type="checkbox"
+							name="active"
+							id={`${first}_${last}-active`}
+							checked={activepuzzle ? true : false}
+							onChange={() =>
+								setStudentFetchInfo(
+									student,
+									"activepuzzle",
+									!activepuzzle
+								)
+							}
+						/>
+					</div>
 					{!activepuzzle ? renderPuzzleSelect(student) : null}
 				</div>
 				{renderStudentPuzzle(student)}
