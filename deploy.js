9b64e18e69fd8ad815b4ef1168892f06364dea38
Kabirdas Henry <kbrdsk@gmail.com>
2020-11-20 19:03:24 -0800

    created separate repo for production

--- deploy.js
+++ deploy.js
@@ -1,5 +1,15 @@
 const ghpages = require("gh-pages");
 
-ghpages.publish("build", {
-	repo: "git@github.com:kbrdsk/puzzle-app-instructor.git",
-});
+const args = process.argv.slice(2);
+const repo =
+	args[0] === "production"
+		? "git@github.com-mcubmath:mcubmath/instructor.git"
+		: "git@github.com:kbrdsk/puzzle-app-instructor.git";
+
+ghpages.publish(
+	"build",
+	{
+		repo,
+	},
+	(err) => (err ? console.log(err) : console.log(`Published to ${repo}`))
+);
