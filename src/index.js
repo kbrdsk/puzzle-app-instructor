c471a93d54adaf70d7fc025671e329b86949b9cf
Kabirdas Henry <kbrdsk@gmail.com>
2020-12-04 19:11:01 -0800

    no longer fetching directory

--- src/index.js
+++ src/index.js
@@ -6,40 +6,50 @@ import PuzzleDisplay from "./puzzle-display";
 
 import "./stylesheets/index.css";
 
+const puzzleDirectory = {
+	calcudoku: [
+		"4x4beginner1",
+		"4x4beginner2",
+		"4x4beginner3",
+		"4x4beginner4",
+		"4x4beginner5",
+		"4x4intermediate1",
+		"4x4intermediate2",
+		"4x4intermediate3",
+		"4x4intermediate4",
+		"4x4expert1",
+		"4x4expert2",
+		"4x4expert3",
+		"5x5beginner1",
+		"5x5beginner2",
+		"5x5intermediate1",
+	],
+	logic: [
+		"wolfgoatcabbage",
+		"torch",
+		"goldboxes",
+		"10coins",
+		"socks",
+		"childrenboat",
+		"applesandoranges",
+		"averybobbycam",
+		"bear",
+		"hats",
+	],
+	light: [
+		"small",
+		"medium",
+		"large",
+		"smallx",
+		"mediumx",
+		"largex",
+	].flatMap((instance) =>
+		Array.from({ length: 5 }, (...[, i]) => `${instance}${i + 1}`)
+	),
+};
+
 function App() {
 	const [students, setStudents] = useState([]);
-	const [puzzleDirectory, setPuzzleDirectory] = useState({
-		calcudoku: [],
-		logic: [],
-		light: [],
-	});
-	useEffect(() => {
-		(async () => {
-			try {
-				await Promise.all(
-					Object.keys(puzzleDirectory).map(async (puzzleName) => {
-						const instanceListUrl = `${process.env.REACT_APP_API_URL}/puzzles/${puzzleName}`;
-						const response = await fetch(instanceListUrl, {
-							headers: {
-								authorization:
-									process.env.REACT_APP_INSTRUCTOR_PW,
-							},
-						});
-						if (response.ok) {
-							puzzleDirectory[puzzleName] = (
-								await response.json()
-							).map(({ instance }) => instance);
-						} else {
-							console.log(`Http error: ${response.status}`);
-						}
-					})
-				);
-				setPuzzleDirectory({ ...puzzleDirectory });
-			} catch (error) {
-				console.log(error);
-			}
-		})();
-	}, []);
 	return (
 		<div className="app">
 			<StudentList students={students} setStudents={setStudents} />
